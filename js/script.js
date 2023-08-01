9829c23aa4d44023b7286a1691691cd938eb767b
Subhash <79552953+subhashhhhhh@users.noreply.github.com>
2023-08-01 15:30:10 +0530

    committed

--- js/script.js
+++ js/script.js
@@ -1,62 +1,81 @@
-// Function to handle form submission
-document.getElementById("scheduleForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get selected values from the form
-    const year = document.getElementById("year").value;
-    const branch = document.getElementById("branch").value;
-    const subgroup = document.getElementById("subgroup").value;
-  
-    // Check if the schedule is available for the selected subgroup
-    if (isScheduleAvailable(year, branch, subgroup)) {
-      // Call the functions to add to Google Calendar or download ICS file
-      addToGoogleCalendar(year, branch, subgroup);
-      downloadICSFile(year, branch, subgroup);
-    } else {
-      // Display "Not yet available" message and link to GitHub repo
-      displayNotAvailableMessage(subgroup);
-    }
+const yearOptions = [
+  { value: "1", label: "1st Year" },
+  { value: "2", label: "2nd Year" },
+  { value: "3", label: "3rd Year" },
+  { value: "4", label: "4th Year" },
+];
+
+const branchOptions = [
+  { value: "CSE", label: "Computer Science and Engineering (CSE)" },
+  { value: "ECE", label: "Electronics and Communication Engineering (ECE)" },
+];
+
+const subgroupOptions = {
+  CSE: [
+    { value: "CS1", label: "CS1" },
+    { value: "CS2", label: "CS2" },
+  ],
+  ECE: [
+    { value: "ECE1", label: "ECE1" },
+    { value: "ECE2", label: "ECE2" },
+  ],
+};
+
+const yearDropdown = document.getElementById("year");
+yearOptions.forEach((yearOption) => {
+  const option = new Option(yearOption.label, yearOption.value);
+  yearDropdown.appendChild(option);
+});
+
+// Function to populate the branch dropdown based on the selected year
+function populateBranchDropdown(year) {
+  const branchDropdown = document.getElementById("branch");
+  branchDropdown.innerHTML = "";
+
+  const filteredBranchOptions = branchOptions.filter((branchOption) => {
+    return branchOption.value.startsWith(year);
   });
-  
-  // Function to check if the schedule is available for the selected subgroup
-  function isScheduleAvailable(year, branch, subgroup) {
-    // Implement your logic here to check if the schedule is available for the given subgroup
-    // For this example, we'll assume that subgroups "CS1" and "CS2" are available and the rest are not
-    return subgroup === "CS1" || subgroup === "CS2";
+
+  filteredBranchOptions.forEach((branchOption) => {
+    const option = new Option(branchOption.label, branchOption.value);
+    branchDropdown.appendChild(option);
+  });
+
+  branchDropdown.disabled = false;
+  document.getElementById("subgroup").disabled = true;
+}
+
+// Function to populate the subgroup dropdown based on the selected branch
+function populateSubgroupDropdown(branch) {
+  const subgroupDropdown = document.getElementById("subgroup");
+  subgroupDropdown.innerHTML = "";
+
+  const subgroupOptionsForBranch = subgroupOptions[branch] || [];
+
+  subgroupOptionsForBranch.forEach((subgroupOption) => {
+    const option = new Option(subgroupOption.label, subgroupOption.value);
+    subgroupDropdown.appendChild(option);
+  });
+
+  subgroupDropdown.disabled = false;
+}
+
+yearDropdown.addEventListener("change", function () {
+  const selectedYear = this.value;
+  if (selectedYear !== "") {
+    populateBranchDropdown(selectedYear);
+  } else {
+    document.getElementById("branch").disabled = true;
+    document.getElementById("subgroup").disabled = true;
   }
-  
-  // Function to add the schedule to Google Calendar
-  function addToGoogleCalendar(year, branch, subgroup) {
-    // Implement your logic to add the schedule to Google Calendar using the Google Calendar API or other relevant methods
-    // For this example, let's assume an alert for demonstration purposes
-    alert(`Schedule for Year ${year}, Branch ${branch}, Subgroup ${subgroup} has been added to Google Calendar.`);
+});
+
+document.getElementById("branch").addEventListener("change", function () {
+  const selectedBranch = this.value;
+  if (selectedBranch !== "") {
+    populateSubgroupDropdown(selectedBranch);
+  } else {
+    document.getElementById("subgroup").disabled = true;
   }
-  
-  // Function to download the ICS file
-  function downloadICSFile(year, branch, subgroup) {
-    // Implement your logic to generate and download the ICS file
-    // For this example, let's assume an alert for demonstration purposes
-    alert(`ICS file for Year ${year}, Branch ${branch}, Subgroup ${subgroup} has been downloaded.`);
-  }
-  
-  // Function to display "Not yet available" message and link to GitHub repo
-  function displayNotAvailableMessage(subgroup) {
-    const notAvailableMsg = `Schedule for Subgroup ${subgroup} is not yet available.`;
-    const githubLink = "https://github.com/your-organization/your-repo"; // Replace with your GitHub repository link
-  
-    // Create a new paragraph element for the message
-    const messageParagraph = document.createElement("p");
-    messageParagraph.textContent = notAvailableMsg;
-  
-    // Create a new anchor (link) element for the GitHub repo
-    const githubAnchor = document.createElement("a");
-    githubAnchor.textContent = "Add Schedule for this Subgroup (GitHub Repo)";
-    githubAnchor.href = githubLink;
-    githubAnchor.target = "_blank"; // Open link in a new tab
-  
-    // Append the elements to the container
-    const container = document.querySelector(".container");
-    container.appendChild(messageParagraph);
-    container.appendChild(githubAnchor);
-  }
-  
+});
+
