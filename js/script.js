7a80bb8f7df5c32f28cf1111a1c858380d9856ed
Subhash <79552953+subhashhhhhh@users.noreply.github.com>
2023-08-02 20:48:13 +0530

    updated javascript

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ function handleYearButtonClick(event) {
       window.location.href = `years/year${selectedYear}.html`;
     }
   }
+
+  function handleGroupButtonClick(event) {
+    const selectedGroup = event.target.getAttribute("data-group");
+    if (selectedGroup) {
+      window.location.href = `groups/group_${selectedGroup}.html`;
+    }
+
   const yearButtons = document.querySelectorAll(".year-btn");
   yearButtons.forEach((button) => {
     button.addEventListener("click", handleYearButtonClick);
